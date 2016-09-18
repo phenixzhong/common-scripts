e85123fde034d87b6ec93ab5d3d26d29fe9c2112
Phenix <phenixzhong@gmail.com>
2016-09-18 16:39:04 +0800

    Update install_gsutil.sh

--- install_gsutil.sh
+++ install_gsutil.sh
@@ -5,7 +5,7 @@ install_gsutil(){
   export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
   echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-  apt-get update && apt-get install google-cloud-sdk
+  apt-get update && apt-get install google-cloud-sdk -y
   gcloud init
 }
 
