7945f075451439cc56567f71e4aac9551120542d
Phenix <phenixzhong@gmail.com>
2016-09-25 12:07:50 +0800

    Create one_step.sh

---
+++ one_step.sh
@@ -0,0 +1,4 @@
+wget https://github.com/phenixzhong/common-scripts/raw/master/set_system.sh
+sh set_system.sh
+wget https://github.com/phenixzhong/common-scripts/raw/master/install_wordpress.sh
+bash install_wordpress.sh
