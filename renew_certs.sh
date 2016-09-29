2f5501b93cb89b80005279aa097c251bb02559d1
Phenix <phenixzhong@gmail.com>
2016-09-29 13:56:05 +0800

    Update renew_certs.sh

--- renew_certs.sh
+++ renew_certs.sh
@@ -1,12 +1,20 @@
+#!/bin/sh
+
+renew_certs(){
 domain="-d hust.co \
         -d feis.co"
-dir=/root/www/letsencrypt
+dir=/var/www/letsencrypt
 email="--email phenixzhong@gmail.com"
+
 letsencrypt certonly --webroot -w ${dir}  ${email} --agree-tos ${domain} --force-renew
 
 cp -f /etc/letsencrypt/live/hust.co/fullchain.pem /root/www/ssl/
 cp -f /etc/letsencrypt/live/hust.co/privkey.pem /root/www/ssl/
+
 rm /root/www/ssl/sct/*
 /root/tools/ct-submit/ct-submit ct.googleapis.com/aviator </root/www/ssl/fullchain.pem >/root/www/ssl/sct/aviator.sct
 
 nginx -s reload
+}
+
+renew_certs
