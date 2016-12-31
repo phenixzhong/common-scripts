e4371188c21bd54e5e60c8c52ef5c8a499db5313
Phenix <phenixzhong@gmail.com>
2016-12-31 18:25:04 +0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -7,7 +7,7 @@
 
 install_nginx(){
   source_file_path=/var/source_file
-  ngx_pagespeed_version=1.12.34.2 # https://github.com/pagespeed/ngx_pagespeed/releases
+  ngx_pagespeed_version=1.11.33.4 # https://github.com/pagespeed/ngx_pagespeed/releases
   nginx_version=1.11.8            # http://nginx.org/en/download.html
   openssl_version=1_1_0c           # https://github.com/openssl/openssl/releases
   nginx_ct_version=1.3.2          # https://github.com/grahamedgecombe/nginx-ct/releases/latest
