2f98509f9c8258a1dcc2bee554904743e9748ad8
Phenix <phenixzhong@gmail.com>
2017-07-21 20:35:46 +0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -17,11 +17,11 @@ install_nginx(){
   rm -r ${source_file_path}
   mkdir -p ${source_file_path}
   cd ${source_file_path}
-  wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}.zip
-  unzip v${NPS_VERSION}.zip
-  cd ngx_pagespeed-${NPS_VERSION}/
-  NPS_RELEASE_NUMBER=${NPS_VERSION/beta/}
-  NPS_RELEASE_NUMBER=${NPS_VERSION/stable/}
+  wget https://github.com/pagespeed/ngx_pagespeed/archive/v${ngx_pagespeed_version}.zip
+  unzip v${ngx_pagespeed_version}.zip
+  cd ngx_pagespeed-${ngx_pagespeed_version}/
+  NPS_RELEASE_NUMBER=${ngx_pagespeed_version/beta/}
+  NPS_RELEASE_NUMBER=${ngx_pagespeed_version/stable/}
   psol_url=https://dl.google.com/dl/page-speed/psol/${NPS_RELEASE_NUMBER}.tar.gz
   [ -e scripts/format_binary_url.sh ] && psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
   wget ${psol_url}
