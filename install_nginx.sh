ee1bbc3d643259a9bb9e5f16e3a188aed5127a20
Phenix <phenixzhong@gmail.com>
2020-02-01 22:44:21 +0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -22,7 +22,7 @@ install_nginx(){
   unzip v${NPS_VERSION}.zip
   nps_dir=$(find . -name "*pagespeed-ngx-${NPS_VERSION}" -type d)
   cd "$nps_dir"
-  NPS_RELEASE_NUMBER=${NPS_VERSION/beta/}
+ # NPS_RELEASE_NUMBER=${NPS_VERSION/beta/}
   NPS_RELEASE_NUMBER=${NPS_VERSION/stable/}
   psol_url=https://dl.google.com/dl/page-speed/psol/${NPS_RELEASE_NUMBER}.tar.gz
   [ -e scripts/format_binary_url.sh ] && psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
