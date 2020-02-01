e7f3c795c83e0faed646509fb7f2c59b1bb8c2f5
Phenix <phenixzhong@gmail.com>
2020-02-02 02:38:08 +0800

    Update install_nginx_with_ngx.sh

--- install_nginx_with_ngx.sh
+++ install_nginx_with_ngx.sh
@@ -19,8 +19,7 @@ install_nginx(){
   
   git clone git@github.com:pagespeed/ngx_pagespeed.git
   cd ngx_pagespeed/
-  scripts/build_ngx_pagespeed.sh \
-  -n ${NGINX_VERSION} -v latest  -a '--prefix=/etc/nginx \
+  scripts/build_ngx_pagespeed.sh -n ${NGINX_VERSION} -v latest  -a '--prefix=/etc/nginx \
   --sbin-path=/usr/sbin/nginx \
   --modules-path=/etc/nginx/modules \
   --conf-path=/etc/nginx/nginx.conf \
