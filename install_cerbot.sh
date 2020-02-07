766a81562d3be03384a901614292951ac779ba21
Phenix <phenixzhong@gmail.com>
2020-02-07 14:18:07 +0800

    Update install_cerbot.sh

--- install_cerbot.sh
+++ install_cerbot.sh
@@ -1,9 +1,9 @@
 apt-get update
-apt-get install software-properties-common
+apt-get install software-properties-common -y
 add-apt-repository universe
 add-apt-repository ppa:certbot/certbot
 apt-get update
 
-apt-get install certbot python-certbot-nginx
+apt-get install certbot python-certbot-nginx -y
 
-apt-get install python3-certbot-dns-cloudflare
+apt-get install python3-certbot-dns-cloudflare -y
